40d2025f079c8acf1848e57d10899625ba2836d6
Jackson <jacksonfa7@gmail.com>
2018-05-02 22:32:50 -0300

    Update service.py

--- service.py
+++ service.py
@@ -2,117 +2,103 @@ from flask import Flask, flash, abort, redirect, url_for, request, render_templa
 import os, sys
 import config
 import boto.ec2.elb
-import boto
+
+import re
 from boto.ec2 import *
 
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
 
 	list = []
 	creds = config.get_ec2_conf()
-
 	for region in config.region_list():
 		conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-		zones = conn.get_all_zones()	
-		instances = conn.get_all_instance_status()
-		instance_count = len(instances)
-		ebs = conn.get_all_volumes()
-		ebscount = len(ebs)
-		unattached_ebs = 0
-		unattached_eli = 0
-		event_count = 0
-	
-		for instance in instances:
-			events = instance.events
-			if events:
-				event_count = event_count + 1	
+		reservations = conn.get_all_reservations()
 
-		for vol in ebs:
-			state = vol.attachment_state()
-			if state == None:
-				unattached_ebs = unattached_ebs + 1
+		for i in range(0, len(reservations)):
+			try:
+				metadata = reservations[i].instances[0]
+				name = re.sub(r'{u\'Name\': u\'', "", str(metadata.tags))
+				metadata_name = str(re.sub(r'\'}', "", name))
+				metadata_server_id = str(metadata.id)
+				metadata_instance_type = str(metadata.instance_type)
+				metadata_instance_state = str(metadata.state)
+			except:
+				metadata_name = ""
+				metadata_server_id = ""
+				metadata_instance_type = ""
+				metadata_instance_state = ""
 
-		elis = conn.get_all_addresses()
-		eli_count = len(elis)
-
-
-		for eli in elis:
-			instance_id = eli.instance_id
-			if not instance_id:
-				unattached_eli = unattached_eli + 1
-
-		connelb = boto.ec2.elb.connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-		elb = connelb.get_all_load_balancers()
-		elb_count = len(elb)
-		list.append({ 'region' : region, 'zones': zones, 'instance_count' : instance_count, 'ebscount' : ebscount, 'unattached_ebs' : unattached_ebs, 'eli_count' : eli_count, 'unattached_eli' : unattached_eli, 'elb_count' : elb_count, 'event_count' : event_count})
-		
+			list.append({ 
+				'name' : metadata_name,
+				'server_id' : metadata_server_id,
+				'instance_type' : metadata_instance_type,
+				'instance_state' : metadata_instance_state,
+				'instance_region' : region
+			})
 	return render_template('index.html',list=list)
 
-@app.route('/ebs_volumes/<region>/')
-def ebs_volumes(region=None):
+@app.route('/volumes/')
+def volumes():
 	creds = config.get_ec2_conf()
-	conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-	ebs = conn.get_all_volumes()
-	ebs_vol = []	
-	for vol in ebs:
-		state = vol.attachment_state()
-		if state == None:
-			ebs_info = { 'id' : vol.id, 'size' : vol.size, 'iops' : vol.iops, 'status' : vol.status }
-			ebs_vol.append(ebs_info)
-	return render_template('ebs_volume.html',ebs_vol=ebs_vol,region=region)
-			
-@app.route('/ebs_volumes/<region>/delete/<vol_id>')
-def delete_ebs_vol(region=None,vol_id=None):
-	creds = config.get_ec2_conf()	
-	conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-	vol_id = vol_id.encode('ascii')
-	vol_ids = conn.get_all_volumes(volume_ids=vol_id)
-	for vol in vol_ids:
-		vol.delete()
-	return redirect(url_for('ebs_volumes', region=region))
-	
-@app.route('/elastic_ips/<region>/')
-def elastic_ips(region=None):
-	creds = config.get_ec2_conf()
-	conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-	elis = conn.get_all_addresses()
-	un_eli = []
-	for eli in elis:
-		instance_id = eli.instance_id
-		if not instance_id:
-			eli_info = { 'public_ip' : eli.public_ip, 'domain' : eli.domain}
-			un_eli.append(eli_info)
-	return render_template('elastic_ip.html',un_eli=un_eli,region=region)
+	for region in config.region_list():
+		conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+		volumes = conn.get_all_volumes()
+		volume_list = []
+		for volume in volumes:
+			name = re.sub(r'{u\'Name\': u\'', "", str(volume.tags))
+			name = str(re.sub(r'\'}', "", name))	
+			volume_info = { 'name' : name,
+							'id' : volume.id, 
+							'size' : volume.size, 
+							'status' : volume.status, 
+							'type' : volume.type, 
+							'attachment_state' : volume.attach_data.instance_id,
+							'volume_region' : region }
+			volume_list.append(volume_info)
 
-@app.route('/elastic_ips/<region>/delete/<ip>')
-def delete_elastic_ip(region=None,ip=None):
-	creds = config.get_ec2_conf()
-	conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-	ip = ip.encode('ascii')
-	elis = conn.get_all_addresses(addresses=ip)
+	return render_template('volumes.html',volume_list=volume_list)
 
-	for eli in elis:
-		eli.release()
-	return redirect(url_for('elastic_ips', region=region))
-	
-
-@app.route('/instance_events/<region>/')
-def instance_events(region=None):
+@app.route('/stop_instance/<instance_id>/')
+def stop_instance(instance_id=None):
 	creds = config.get_ec2_conf()
-	conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
-	instances = conn.get_all_instance_status()
-	instance_event_list = []
-	for instance in instances:
-		event = instance.events
-		if event:
-			event_info = { 'instance_id' : instance.id, 'event' : instance.events[0].code, 'description' : instance.events[0].description, 'event_before' : instance.events[0].not_before, 'event_after': instance.events[0].not_after }
-			instance_event_list.append(event_info)
-	return render_template('instance_events.html', instance_event_list=instance_event_list)
-			
+	for region in config.region_list():
+		conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+		try:
+			conn.stop_instances(instance_ids=[instance_id])
+		except:
+			print "The instance " + str(instance_id) + " is not in this region.Checking next region " + str(region)
+	return index()
+
+@app.route('/start_instance/<instance_id>/')
+def start_instance(instance_id=None):
+	creds = config.get_ec2_conf()
+	for region in config.region_list():
+		conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+		try:
+			instances = conn.get_only_instances(instance_ids=[instance_id])
+			instances[0].start()
+		except:
+			print "The instance " + str(instance_id) + " is not in this region. Checking next region " + str(region)
+	return index()
+
+@app.route('/delete_volume/<volume_id>')
+def delete_volume(volume_id=None):
+	creds = config.get_ec2_conf()
+	for region in config.region_list():
+		conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+		volume_id = volume_id.encode('ascii')
+		try:
+			vololume_ids = conn.get_all_volumes(volume_ids=volume_id)
+			for volume in vololume_ids:
+    				volume.delete()
+		except:
+			print "The volume " + str(volume_id) + " is not in this region. Checking next region " + str(region)
+	return volumes()
+		
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0')
